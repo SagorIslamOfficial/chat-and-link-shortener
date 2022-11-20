aee19f35a06d7a070e82dfb7de87324463de75f0
“SagorIslamOfficial” <alaminislam1274@gmail.com>
2022-11-20 08:08:22 +0600

    redirect to #chatify url after login/register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,15 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
+Route::get('/', function () {return view('welcome');});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
